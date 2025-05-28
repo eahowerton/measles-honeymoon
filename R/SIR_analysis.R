db5905f500c546782ee71f48ae306d3d52fa4e54
Emily Howerton <46577370+eahowerton@users.noreply.github.com>
2025-05-28 10:08:17 -0400

    add constant importation to model

--- R/SIR_analysis.R
+++ R/SIR_analysis.R
@@ -14,8 +14,8 @@ seirmod2 = function(t, y, parameters, vax_pct) {
   parameters = c(parameters, v = vax_pct(t))
   with(as.list(parameters), {
     beta = beta0 * (1 + beta1 * cos(2 * pi * t))
-    dS = mu * (N - S) * (1 - v) - beta * S * I/N
-    dE = beta * S * I/N - (mu + sigma) * E
+    dS = mu * (N - S) * (1 - v) - beta * S * I/N - delta * S # additional importations
+    dE = beta * S * I/N - (mu + sigma) * E + delta * S
     dI = sigma * E - (mu + gamma) * I
     dR = mu * (N - S) * v + gamma * I - mu * R
     res = c(dS, dE, dI, dR)
@@ -27,7 +27,7 @@ seirmod2 = function(t, y, parameters, vax_pct) {
 times = seq(0, 100, by = 1/365)
 no_vax = approxfun(times, rep(0, length(times)))
 paras = c(mu = 1/50, N = 1, beta0 = 1000, beta1 = 0.2,
-          sigma = 365/8, gamma = 365/5, vax_pct = 0)
+          sigma = 365/8, gamma = 365/5, vax_pct = 0, delta = 0)
 start = c(S = 0.06, E = 0, I = 0.001, R = 0.939)
 results = as.data.frame(ode(start, times, seirmod2, paras, vax_pct = no_vax))
 
@@ -133,6 +133,7 @@ for(i in 1:length(examp_beta1)){
            Re = paras_tmp["beta0"] * (1 + paras_tmp["beta1"] * cos(2 * pi * time)) * paras_tmp["sigma"] * S / ((paras_tmp["gamma"] + paras_tmp["mu"]) * (paras_tmp["sigma"] + paras_tmp["mu"])  * paras_tmp["N"]))
 }
 
+
 rslts_all = bind_rows(vax_peak_results_grad) %>%
   mutate(vax_start = "peak", honeymoon_end = "gradual") %>%
   bind_rows(bind_rows(vax_trough_results_grad) %>% mutate(vax_start = "trough", honeymoon_end = "gradual")) %>%
@@ -140,7 +141,8 @@ rslts_all = bind_rows(vax_peak_results_grad) %>%
   bind_rows(bind_rows(vax_peak_results) %>% mutate(vax_start = "peak", honeymoon_end = "jump")) %>%
   mutate(stage = ifelse(time < ifelse(vax_start == "peak", time_on_peak, time_on_trough), "pre vax", 
                         ifelse(time < ifelse(vax_start == "peak", time_on_peak, time_on_trough) + nyear_vax, "high vax", "decline vax")), 
-         time = ifelse(vax_start == "trough", time - 1, time))
+         time = ifelse(vax_start == "trough", time - 1, time)
+         ) 
 
 
 p1 = data.frame(
@@ -180,11 +182,11 @@ p2 = rslts_all %>%
 
 cowplot::plot_grid(p1, p2, ncol = 1, align = "v", axis = "lr", rel_heights = c(0.3, 0.7))
 
-
 rslts_all %>%
   filter(time > 77, time < 85) %>%
   ggplot(aes(x = log(S), y = log(I), color = stage, linetype = vax_start)) + 
   geom_path() + 
+  ggtitle("without importation") + 
   facet_grid(rows = vars(beta1), cols = vars(honeymoon_end)) + 
   scale_color_manual(values = c("blue", "red", "black")) + 
   scale_linetype_manual(values = c("dashed", "solid")) +
@@ -200,6 +202,7 @@ rslts_all %>%
   # geom_rect() +
   geom_path() + 
   facet_grid(rows = vars(beta1), cols = vars(honeymoon_end)) + 
+  ggtitle("without importation") + 
   scale_color_manual(values = c("blue", "red", "black")) + 
   scale_linetype_manual(values = c("dashed", "solid")) + 
   theme_bw() + 
@@ -352,3 +355,134 @@ rslts_all_update_IC %>%
   theme(legend.position = "bottom", 
         panel.grid = element_blank())
   
+#### ADD SMALL AMOUNT OF IMPORTATION -------------------------------------------
+paras_importation = c(mu = 1/50, N = 1, beta0 = 1000, beta1 = 0.2,
+          sigma = 365/8, gamma = 365/5, vax_pct = 0, delta = 1e-4) # delta chosen arbitrarily for now
+
+results_importation = as.data.frame(ode(start, times, seirmod2, paras_importation, vax_pct = no_vax))
+
+ggplot(data = results %>% filter(time > 95), aes(x = time, y = I)) + 
+  geom_line() + 
+  geom_line(data = results_importation %>% filter(time > 95), color = "red")
+
+# quick bifurcation diagram again
+bifur_reslts_import = list()
+# Loop over beta1’s
+for (i in 1:length(beta1_vec)) {
+  paras_tmp = paras_importation
+  paras_tmp["beta1"] = beta1_vec[i]
+  bifur_reslts_import[[i]] = as.data.frame(ode(start, times, seirmod2, paras_tmp, vax_pct = no_vax)) %>%
+    mutate(beta1 = beta1_vec[i])
+}
+
+p1 = bind_rows(bifur_reslts_import) %>%
+  filter(time%%1 == 0, time > 80, !is.na(I)) %>%
+  ggplot(aes(x = beta1, y = I)) + 
+  geom_point(alpha = 0.2) + 
+  geom_vline(data = data.frame(beta1 = examp_beta1), 
+             aes(xintercept = beta1), linetype = "dotted", color = 'red') + 
+  theme_bw()
+p2 = bind_rows(bifur_reslts_import) %>%
+  filter(round(beta1, 5) %in% examp_beta1, time > 90) %>%
+  ggplot(aes(x = time, y = I)) + 
+  geom_line() + 
+  facet_wrap(vars(beta1), ncol = 1) + 
+  theme_bw()
+p3 = bind_rows(bifur_reslts_import) %>%
+  filter(round(beta1, 5) %in% examp_beta1, time > 94) %>%
+  ggplot(aes(x = S, y = I)) + 
+  geom_path() + 
+  facet_wrap(vars(beta1), ncol = 1) + 
+  theme_bw()
+cowplot::plot_grid(p1, p2, p3, nrow = 1)
+
+### now run through different vaccination scenarios
+
+vax_import_peak_results = list()
+vax_import_trough_results = list()
+vax_import_peak_results_grad = list()
+vax_import_trough_results_grad = list()
+for(i in 1:length(examp_beta1)){
+  paras_tmp = paras_importation
+  paras_tmp["beta1"] = examp_beta1[i]
+  # jump peak
+  vax_import_peak_results[[i]] = as.data.frame(
+    ode(start, times, seirmod2, paras_tmp, vax_pct = vax_peakstart, rtol = 1e-10, atol = 1e-10)) %>%
+    mutate(beta1 = examp_beta1[i], 
+           # Re = c(0, diff(Re)), 
+           Re = paras_tmp["beta0"] * (1 + paras_tmp["beta1"] * cos(2 * pi * time)) * paras_tmp["sigma"] * S / ((paras_tmp["gamma"] + paras_tmp["mu"]) * (paras_tmp["sigma"] + paras_tmp["mu"])  * paras_tmp["N"]))
+  # jump trough
+  vax_import_trough_results[[i]] = as.data.frame(
+    ode(start, times, seirmod2, paras_tmp, vax_pct = vax_troughstart)) %>%
+    mutate(beta1 = examp_beta1[i], 
+           Re = paras_tmp["beta0"] * (1 + paras_tmp["beta1"] * cos(2 * pi * time)) * paras_tmp["sigma"] * S / ((paras_tmp["gamma"] + paras_tmp["mu"]) * (paras_tmp["sigma"] + paras_tmp["mu"])  * paras_tmp["N"]))
+  # gradual peak
+  vax_import_peak_results_grad[[i]] = as.data.frame(
+    ode(start, times, seirmod2, paras_tmp, vax_pct = vax_peakstart_grad)) %>%
+    mutate(beta1 = examp_beta1[i], 
+           Re = paras_tmp["beta0"] * (1 + paras_tmp["beta1"] * cos(2 * pi * time)) * paras_tmp["sigma"] * S / ((paras_tmp["gamma"] + paras_tmp["mu"]) * (paras_tmp["sigma"] + paras_tmp["mu"])  * paras_tmp["N"]))
+  # gradual trough
+  vax_import_trough_results_grad[[i]] = as.data.frame(
+    ode(start, times, seirmod2, paras_tmp, vax_pct = vax_troughstart_grad)) %>%
+    mutate(beta1 = examp_beta1[i], 
+           Re = paras_tmp["beta0"] * (1 + paras_tmp["beta1"] * cos(2 * pi * time)) * paras_tmp["sigma"] * S / ((paras_tmp["gamma"] + paras_tmp["mu"]) * (paras_tmp["sigma"] + paras_tmp["mu"])  * paras_tmp["N"]))
+}
+
+rslts_all_import = bind_rows(vax_import_peak_results_grad) %>%
+  mutate(vax_start = "peak", honeymoon_end = "gradual") %>%
+  bind_rows(bind_rows(vax_import_trough_results_grad) %>% mutate(vax_start = "trough", honeymoon_end = "gradual")) %>%
+  bind_rows(bind_rows(vax_import_trough_results) %>% mutate(vax_start = "trough", honeymoon_end = "jump")) %>%
+  bind_rows(bind_rows(vax_import_peak_results) %>% mutate(vax_start = "peak", honeymoon_end = "jump")) %>%
+  mutate(stage = ifelse(time < ifelse(vax_start == "peak", time_on_peak, time_on_trough), "pre vax", 
+                        ifelse(time < ifelse(vax_start == "peak", time_on_peak, time_on_trough) + nyear_vax, "high vax", "decline vax")), 
+         time = ifelse(vax_start == "trough", time - 1, time), 
+         # switch peak and trough for beta1 = 0.2 in this case
+         vax_start = ifelse(beta1 == 0.2, ifelse(vax_start == "peak", "trough", "peak"), vax_start))
+
+
+rslts_all_import %>%
+  filter(time > 77) %>%
+  ggplot() + 
+  geom_rect(data = data.frame(t = c(time_on_peak),
+                              tend = c(time_on_peak) + nyear_vax,
+                              vax_start = c("peak")), 
+            aes(xmin = t, xmax = tend, ymin = 0, ymax = Inf), alpha = 0.2) +
+  geom_vline(data = data.frame(t = seq(80, 100, 5)), aes(xintercept = t), linetype = "dotted") + 
+  geom_line(aes(x = time, y = I, color = stage, linetype = vax_start)) + 
+  ggtitle("with importation") + 
+  facet_grid(rows = vars(beta1), cols = vars(honeymoon_end)) + 
+  scale_color_manual(values = c("blue", "red", "black")) + 
+  scale_linetype_manual(values = c("longdash", "solid")) + 
+  theme_bw() + 
+  theme(legend.position = "bottom", 
+        panel.grid = element_blank())
+
+rslts_all_import %>%
+  filter(time > 77, time < 85) %>%
+  ggplot(aes(x = log(S), y = log(I), color = stage, linetype = vax_start)) + 
+  geom_path() + 
+  ggtitle("with importation") + 
+  facet_grid(rows = vars(beta1), cols = vars(honeymoon_end)) + 
+  scale_color_manual(values = c("blue", "red", "black")) + 
+  scale_linetype_manual(values = c("dashed", "solid")) +
+  theme_bw() + 
+  theme(legend.position = "bottom", 
+        panel.grid = element_blank())
+
+rslts_all_import %>%
+  filter(time > 77, time < 85) %>%
+  ggplot(aes(x = log(Re), y = log(I), color = stage, linetype = vax_start)) + 
+  # geom_rect() +
+  geom_path() + 
+  geom_vline(xintercept = 0, linetype = "dotted") + 
+  ggtitle("with importation") + 
+  facet_grid(rows = vars(beta1), cols = vars(honeymoon_end)) + 
+  scale_color_manual(values = c("blue", "red", "black")) + 
+  scale_linetype_manual(values = c("dashed", "solid")) + 
+  theme_bw() + 
+  theme(legend.position = "bottom", 
+        panel.grid = element_blank())
+
+## hmm that changes things, but doesn't seem to have any clearer patterns
+## which suggests that it is not small levels of I alone, that are driving the
+## transient dynamics
