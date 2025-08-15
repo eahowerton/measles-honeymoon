8ec7960f646edbd4865275cabb706333d251e527
Emily Howerton <46577370+eahowerton@users.noreply.github.com>
2025-08-15 10:20:46 -0400

    Start to simulate pre and post rebound

--- R/age-structure_deterministic/2_find-scalars.R
+++ R/age-structure_deterministic/2_find-scalars.R
@@ -21,8 +21,11 @@ compartments = c("S", "I", "R")
 
 R0 <- 14
 paras = c(mu = 1/50, N = 500000, beta1 = 0,
-          gamma = 365/14, delta = 0, p = 0) # delta = 1e-4
+          gamma = 365/14, delta = 1e-4, p = 0) # delta = 1e-4
 paras["beta0"]  = (paras["gamma"] + paras["mu"])*R0
+
+paras_noimport = paras
+paras_noimport["delta"] = 0
 
 # R0
 with(as.list(paras), beta0/(gamma + mu))
@@ -30,25 +33,126 @@ with(as.list(paras), beta0/(gamma + mu))
 start_vax = c(0.9, 0.92, 0.94, 0.96)
 
 #### GET R0 VALUES FOR EACH WAIFW ----------------------------------------------
-stable_age = findStableStruct(age_classes, mort, fert)$stable.age
+stable_age = findStableStruct(age_classes, mort, fert, 1/52)$stable.age
 
-get_Rt(waifw[[1]], stable_age, paras["beta0"], paras["gamma"], paras["N"])
-
-#### FIND SCALARS --------------------------------------------------------------
-# each WAIFW matrix yields a different equilibrium number of infections 
-scalars_v = vector("list", length(start_vax))
-for(i in 1:length(start_vax)){
-  print(paste0("start vax = ", start_vax[i]))
-  scalars_v[[i]] = match_on_age_or_inc(age_classes = age_classes, mort = mort, 
-                                       fert = fert, start_pop = paras["N"], 
-                                       compartments = compartments, 
-                                       vax_pct = start_vax[i], max_t = 200, 
-                                       params = paras, waifws = waifw[-1])
+find_scalar = function(s, R0, waifw, S, beta0, gamma, N){
+  # print(paste0("s: ", s, " R0: ", get_Rt(waifw, S, beta0*s, gamma, N)))
+  diff = get_Rt(waifw, S, beta0*s, gamma, N) - R0
+  return(abs(diff))
 }
 
-scalars_v_full =  bind_rows(scalars_v, .id = "vax_id") %>%
-  mutate(vax_id = as.integer(vax_id)) %>%
-  left_join(data.frame(vax_id = 1:length(start_vax), 
-                       v = start_vax))
+scalars = data.frame(waifw_id = 1:5, scalar = NA, diff = NA)
+for(i in 1:length(waifw)){
+  o = optimize(f = find_scalar, tol = 1e-8, interval = c(0, 100), R0 = R0,
+               waifw = waifw[[i]], S = stable_age, beta0 = paras["beta0"], gamma = paras["gamma"], N = 1)
+  print(get_Rt(waifw[[i]], stable_age, paras["beta0"]*o$minimum, paras["gamma"], 1))
+  scalars[i, 2:3] = c(o$minimum, o$objective)
+}
 
-saveRDS(scalars_v_full, "data/output-data/scalars_by_v.rds")
+# create a list of parameters for 
+paras_all = lapply(1:5, function(i){paras_tmp = paras; paras_tmp["beta0"] = paras_tmp["beta0"]*scalars[i,2]; return(paras_tmp)})
+paras_all_noimport = lapply(1:5, function(i){paras_tmp = paras_noimport; paras_tmp["beta0"] = paras_tmp["beta0"]*scalars[i,2]; return(paras_tmp)})
+
+#### CONFIRM EQUILIBRIA ARE CLOSE, AFTER USING THESE SCALARS -------------------
+vax_equilib = vector("list", length(waifw))
+for(j in 1:length(start_vax)){
+  print(paste0("j: ", j, "/", length(start_vax)))
+  tmp = vector("list", length(waifw))
+  for(i in 1:length(waifw)){
+    print(i)
+    Fmat = buildFMatrix(age.classes = age_classes, fert = fert, ncompartments = length(compartments))
+    IC = setup_IC(start_pop = paras["N"], age_classes = age_classes, 
+                  compartments = compartments, mort = mort, fert = fert, 
+                  IC_type = "stable-age", dt = 1/52)
+    t = runsteady(
+      y = IC, times = c(0, 750), func = sir_age_structured, parms = paras_all[[i]], # runsteady arguments
+      compartments = compartments, age_classes = age_classes, mort = mort, fert = fert,
+      vax_change_times = c(0), vax_rates = c(start_vax[j]),
+      waifw = waifw[[i]], length(age_classes), 
+      Fmat = Fmat, adjust_beta_flag = FALSE, print_warnings_flag = FALSE
+      )
+    tmp[[i]] = data.frame(variable = names(t$y), 
+                          value = t$y, 
+                          time = attr(t, "time"),
+                          steady = attr(t, "steady"), 
+                          waifw_id = i, 
+                          start_vax = start_vax[j])
+  }
+  vax_equilib[[j]] = bind_rows(tmp)
+}
+
+vax_equilib_long = bind_rows(vax_equilib) %>%
+  mutate(age = ifelse(substr(variable, 1, 1) %in% compartments, 
+                      as.double(substr(variable, 3, nchar(variable))), NA), 
+         variable = ifelse(substr(variable, 1, 1) %in% compartments, substr(variable, 1, 1), variable)) %>%
+  mutate(age = round(age, 4)) %>%
+  left_join(data.frame(age = round(age_classes, 4), 
+                       bin_width = bin_width))
+
+# plot results to do some checks
+vax_equilib_long %>%
+  filter(variable %in% c("S", "I", "R")) %>%
+  summarize(value = sum(value), .by = c("waifw_id", "start_vax", "variable")) %>%
+  ggplot(aes(x = start_vax, y = value, color = as.factor(waifw_id))) + 
+  geom_point() + 
+  geom_line() +
+  facet_wrap(vars(variable), scales = "free") + 
+  theme_bw()
+  
+vax_equilib_long %>%
+  filter(variable == "I") %>%
+  mutate(value_scaled = value/bin_width) %>%
+  ggplot(aes(x = age, y = value_scaled, color = as.factor(waifw_id))) +
+  geom_line() + 
+  facet_wrap(vars(start_vax), scales = 'free') +
+  theme_bw()
+
+#### SIMULATE VACCINATION RELEASE FROM EQUILIBRIUM -----------------------------
+# 1. remove all infections from the population (assume local extinction)
+# 2. simulate accumulation of susceptibles over time at different release vax levels
+release_vax = c(0, 0.4, 0.8)
+nyears_postrelease = 12
+chosen_dt = 1/52
+
+# generate data.frame of simulations to run
+# note: do not need to repeat for all WAIFWs because there are no new infections
+tst_release = expand.grid(start_vax = start_vax, 
+                         release_vax = release_vax)
+
+susc_after_release = vector("list", nrow(tst_release))
+for(i in 1:nrow(tst_release)){
+  print(paste0("i: ", i, "/", nrow(tst_release)))
+  tmp_start_vax = tst_release[i, "start_vax"]
+  tmp_release_vax = tst_release[i, "release_vax"]
+  IC_manual = vax_equilib_long %>% filter(start_vax == tmp_start_vax, waifw_id == 1, !(variable %in% c("BH", "BHs", "BHi", "BHb")))
+  names_IC = paste(IC_manual$variable, IC_manual$age, sep = "_")
+  IC_manual = IC_manual$value
+  names(IC_manual) = names_IC
+  # now move all infections into R (local extinction)
+  I_indx = which(substr(names(IC_manual), 1, 1) == "I")
+  R_indx = which(substr(names(IC_manual), 1, 1) == "R")
+  IC_manual[R_indx] = IC_manual[R_indx] + IC_manual[I_indx]
+  IC_manual[I_indx] = 0
+  susc_after_release[[i]] = run_ode(
+    age_classes = age_classes, mort = mort, fert = fert, start_pop = paras["N"],
+    compartments = compartments, dt = chosen_dt, waifw = waifw[[1]],
+    vax_change_times = c(0), vax_rates = c(tmp_release_vax), 
+    IC_type = "manual", IC_manual = IC_manual, max_t = nyears_postrelease, 
+    params = paras_all_noimport[[1]],
+    adjust_beta_flag = FALSE, plot_flag = FALSE
+  )
+}
+beep()
+
+susc_after_release_long = bind_rows(susc_after_release, .id = "sim_id") %>%
+  mutate(sim_id = as.integer(sim_id)) %>%
+  left_join(tst_release %>% mutate(sim_id = seq_len(dplyr::n())))
+
+susc_after_release_long %>% 
+  filter(variable == "S") %>%
+  summarize(value = sum(value), .by = c("time", "start_vax", "release_vax")) %>%
+  ggplot(aes(x = time, y = value, color = as.factor(release_vax))) + 
+  geom_line() + 
+  facet_wrap(vars(start_vax)) + 
+  theme_bw()
+
